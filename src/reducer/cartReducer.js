fd6218ed56b91690b7e88f02449e608ae390f170
Chittesh-Kumar <128819733+ChitteshK139@users.noreply.github.com>
2024-03-29 20:31:27 +0530

    Commit-3

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -6,23 +6,100 @@ const cartReducer = (state, action) => {
     //   product
     // );
 
-    let cartProduct;
+    // Tackle the existing product
 
-    cartProduct = {
-      id: id + color,
-      name: product.name,
-      color,
-      amount,
-      image: product.image[0].url,
-      price: product.price,
-      max: product.stock,
-    };
+    let existingProduct=state.cart.find(
+      (curItem)=> curItem.id =id,color
+      );
 
-    return {
-      ...state,
-      cart: [...state.cart, cartProduct],
-    };
+    console.log(
+          "🚀 ~ file: cartReducer.js ~ line 15 ~ cartReducer ~ existingProduct",
+          existingProduct
+    );
+
+    if(existingProduct){
+        let updatedProduct=state.cart.map((curElem)=>{
+          if (curElem.id==id+color){
+            let newAmt=curElem.amount+amount;
+  
+            if (newAmt>=curElem.max){
+              newAmt=curElem.max;   
+            }
+            return{
+              ...curElem,
+                amount:newAmt,
+            };
+          }
+          else{
+            return curElem;
+          }
+        });
+        return {
+          ...state,
+          cart: updatedProduct,
+        }; 
+    }
+    else{
+      let cartProduct;
+
+      cartProduct = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.image[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+
+      return {
+        ...state,
+        cart: [...state.cart, cartProduct],
+      };
+    }
   }
+
+  // To set increment and decrement
+  if (action.type === "SET_DECREMENT") {
+    let updatedProduct=state.cart.map((curElem)=>{
+      if (curElem.id==action.payload){
+        let decAmt=curElem.amount-1;
+
+        if (decAmt<=1){
+          decAmt=1;
+        }
+        return {
+          ...curElem,
+          amount:decAmt,
+        };
+      }
+      else{
+        return curElem;
+      }
+    });
+    return {...state,cart:updatedProduct};
+  }
+  
+  if (action.type === "SET_INCREMENT") {
+    let updatedProduct=state.cart.map((curElem)=>{
+      if (curElem.id==action.payload){
+        let incAmt=curElem.amount+1;
+
+        if (decAmt>=curElem.max){
+          incAmt=curElem.max;
+        }
+        return {
+          ...curElem,
+          amount:incAmt,
+        };
+      }
+      else{
+        return curElem;
+      }
+    });
+    return {...state,cart:updatedProduct};
+  }
+
 
   if (action.type === "REMOVE_ITEM") {
     let updatedCart = state.cart.filter(
@@ -39,6 +116,20 @@ const cartReducer = (state, action) => {
     return {
       ...state,
       cart:[],
+    }
+  }
+
+  if(action.type ==="CART_TOTAL_ITEM"){
+    let updatedItemval=state.cart.reduce((initialVal,curElem)=>{
+      let {amount}=curElem;
+  
+      initialVal=initialVal+amount;
+      return initialVal;
+    },0);
+  
+    return{
+      ...state,
+      total_item:updatedItemval,
     }
   }
 
